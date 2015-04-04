35dd0ada6fdd0bac20e9f7600083435aa5494490
latril_m <martin.latrille@epita.fr>
2015-04-04 13:50:16 +0200

    added custom test package handling

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Python import
-import re, sys, os
+import re, sys, os, argparse
 
 sys.path.append(os.path.realpath(__file__))
 
@@ -9,13 +9,20 @@ sys.path.append(os.path.realpath(__file__))
 import core, settings
 from colors import printout
 
-def getTestSets():
+def getTestSets(package):
   """
   Get test sets from files in ./test_sets/
   """
-  test_sets = []
-  __package__ = __import__('test_sets', level=0)
+  package_path, package_name = package.rsplit('/', 1)
+  sys.path.append(os.path.realpath(package_path))
 
+  try:
+    __package__ = __import__(package_name)
+  except:
+    print 'No such package.'
+    sys.exit(1)
+
+  test_sets = []
   for mod_name in dir(__package__):
     __module__ = getattr(__package__, mod_name)
     for class_name in dir(__module__):
@@ -26,7 +33,14 @@ def getTestSets():
   return test_sets
 
 if __name__ == "__main__":
-  test_sets = getTestSets()
+  parser = argparse.ArgumentParser(description="Ultra-lightweight test suite focused on REST API continuous integration.")
+  parser.add_argument('-p', '--package', help='the package containing your test sets')
+  args = parser.parse_args()
+
+  if args.package:
+    test_sets = getTestSets(args.package)
+  else:
+    sys.exit(1)
 
   if len(test_sets) != 0:
     app = core.App()
