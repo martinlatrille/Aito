94decbafddb456689d1a6fd175be6cd81ee148c4
latril_m <martin.latrille@epita.fr>
2015-05-09 17:07:14 +0200

    separated printing from execution

--- src/core.py
+++ src/core.py
@@ -5,7 +5,7 @@ import re, requests
 
 # Local imports
 import settings
-from colors import printout
+import printers
 
 class TestSet:
   """
@@ -64,63 +64,11 @@ class App:
   """
   Main entry
   """
-  def __init__(self, verbosity):
+  def __init__(self, verbosity): #, output):
     self.verbosity = verbosity
 
-  def printErrorNoSetFound(self):
-    """
-    Print 'ErrorNoSetFound' error message
-    """
-    print printout(settings.strings['errorNoSetFound'], settings.colors['errors'])
-
-  def printIntro(self):
-    """
-    Print the intro sentence, before testing starts
-    """
-    print printout(settings.strings['intro'], settings.colors['intro'])
-
-  def printSetIntro(self, u):
-    """
-    Print the set intro sentence, before the beginning of each test set
-    """
-    if self.verbosity > 0:
-      print printout(u.__class__.__name__ + ': ' + u.__doc__, settings.colors['setIntro'])
-
-  def printTestOutput(self, output):
-    """
-    Print the output of a test
-    """
-    if self.verbosity > 1:
-      print output
-
-  def printSetResult(self, test_set, nb_tests, nb_ok, total_response_time):
-    """
-    Print set results, after the end of each test set
-    """
-    if self.verbosity > 0:
-      percent = int(100 * (float(nb_ok) / float(nb_tests)))
-      print printout(
-        settings.strings['setResult'].format(nb_tests_passed=nb_ok,
-                                             nb_tests_total=nb_tests,
-                                             percent=percent,
-                                             className=test_set.__class__.__name__),
-        settings.colors['setResult'])
-
-  def printTotalResult(self, nb_tests, nb_ok, total_response_time):
-    """
-    Print total results, after the end of all test sets
-    """
-    percent = int(100 * (float(nb_ok) / float(nb_tests)))
-    print printout(
-      settings.strings['totalResult'].format(nb_tests_passed=nb_ok,
-                                             nb_tests_total=nb_tests,
-                                             percent=percent),
-      settings.colors['totalResult'])
-
-    if percent == 100:
-      print printout(settings.strings['buildOk'], settings.colors['buildOk'])
-    else:
-      print printout(settings.strings['buildKo'], settings.colors['buildKo'])
+    #if output.equals('local'):
+    self.printer = printers.LocalPrinter(verbosity)
 
   def process(self, modules):
     """
@@ -131,14 +79,14 @@ class App:
     data_total['nb_ok'] = 0
 
     if len(modules) == 0:
-      self.printErrorNoSetFound()
+      self.printer.printErrorNoSetFound()
       return
 
-    self.printIntro()
+    self.printer.printIntro()
 
     for test_set in modules:
 
-      self.printSetIntro(test_set)
+      self.printer.printSetIntro(test_set)
 
       data_test_set = {}
       data_test_set['index'] = 0
@@ -156,24 +104,20 @@ class App:
             data_test = func()
 
             if data_test['success']:
-              success = printout(settings.strings['testSuccess'], settings.colors['testSuccess'])
               data_test_set['nb_ok'] += 1
-            else:
-              success = printout(settings.strings['testFailure'], settings.colors['testFailure'])
 
-            output = settings.strings['testOutputFormat'].format(success=success, return_code=data_test['code'], elapsed=data_test['elapsed'], doc=func_doc)
 
+            self.printer.printTestOutput(data_test, func_doc)
           except Exception as e:
-            output = printout(settings.strings['testDirtyFailure'], settings.colors['testDirtyFailure']) + func_doc + str(e)
+            self.printer.printTestDirtyFailure({'success': False, 'dirty': True, 'exception': e})
 
-          self.printTestOutput(output)
 
       test_set.setDown()
 
       data_total['index'] += data_test_set['index']
       data_total['nb_ok'] += data_test_set['nb_ok']
-      self.printSetResult(test_set, data_test_set['index'], data_test_set['nb_ok'], 0)
+      self.printer.printSetResult(test_set, data_test_set['index'], data_test_set['nb_ok'], 0)
 
 
-    self.printTotalResult(data_total['index'], data_total['nb_ok'], 0)
+    self.printer.printTotalResult(data_total['index'], data_total['nb_ok'], 0)
     return 0 if data_total['index'] == data_total['nb_ok'] else 1
---
+++ src/printers.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+# Local import
+import settings
+from colors import printout
+
+class LocalPrinter:
+  def __init__(self, verbosity):
+    self.verbosity = verbosity
+
+  def printErrorNoSetFound(self):
+    """
+    Print 'ErrorNoSetFound' error message
+    """
+    print printout(settings.strings['errorNoSetFound'], settings.colors['errors'])
+
+  def printIntro(self):
+    """
+    Print the intro sentence, before testing starts
+    """
+    print printout(settings.strings['intro'], settings.colors['intro'])
+
+  def printSetIntro(self, u):
+    """
+    Print the set intro sentence, before the beginning of each test set
+    """
+    if self.verbosity > 0:
+      print printout(u.__class__.__name__ + ': ' + u.__doc__, settings.colors['setIntro'])
+
+  def printTestOutput(self, data, doc):
+    """
+    Print the output of a test
+    """
+    if data['success']:
+      success = printout(settings.strings['testSuccess'], settings.colors['testSuccess'])
+    else:
+      success = printout(settings.strings['testFailure'], settings.colors['testFailure'])
+
+    output = settings.strings['testOutputFormat'].format(success=success, return_code=data['code'], elapsed=data['elapsed'], doc=doc)
+
+    if self.verbosity > 1:
+      print output
+
+  def printTestDirtyFailure(self, data):
+    """
+    Print the output of a dirty failed test (aka Exception was thrown during test execution)
+    """
+    output = printout(settings.strings['testDirtyFailure'], settings.colors['testDirtyFailure']) + doc + str(data['exception'])
+
+    if self.verbosity > 1:
+      print output
+
+  def printSetResult(self, test_set, nb_tests, nb_ok, total_response_time):
+    """
+    Print set results, after the end of each test set
+    """
+    if self.verbosity > 0:
+      percent = int(100 * (float(nb_ok) / float(nb_tests)))
+      print printout(
+        settings.strings['setResult'].format(nb_tests_passed=nb_ok,
+                                             nb_tests_total=nb_tests,
+                                             percent=percent,
+                                             className=test_set.__class__.__name__),
+        settings.colors['setResult'])
+
+  def printTotalResult(self, nb_tests, nb_ok, total_response_time):
+    """
+    Print total results, after the end of all test sets
+    """
+    percent = int(100 * (float(nb_ok) / float(nb_tests)))
+    print printout(
+      settings.strings['totalResult'].format(nb_tests_passed=nb_ok,
+                                             nb_tests_total=nb_tests,
+                                             percent=percent),
+      settings.colors['totalResult'])
+
+    if percent == 100:
+      print printout(settings.strings['buildOk'], settings.colors['buildOk'])
+    else:
+      print printout(settings.strings['buildKo'], settings.colors['buildKo'])
