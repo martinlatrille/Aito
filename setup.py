9a44d8a3912cc3e85d0c5109077487a267a86b06
latril_m <martin.latrille@epita.fr>
2015-05-28 15:09:46 +0200

    finished preparing for deploy

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'Aito',
+  packages = ['Aito'], # this must be the same as the name above
+  version = '0.5',
+  description = 'Ultra-lightweight test suite focused on REST API end-to-end tests.',
+  author = 'Martin Latrille',
+  author_email = 'martinlatrille@live.fr',
+  url = 'https://github.com/martinlatrille/Aito', # use the URL to the github repo
+  download_url = 'https://github.com/martinlatrille/Aito/tarball/0.5', # I'll explain this in a second
+  keywords = ['testing', 'REST', 'api', 'end-to-end'], # arbitrary keywords
+  classifiers = [],
+)
