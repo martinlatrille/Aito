dfe56b87dd83130f60a44a329153e5a43398e5b0
Martin Latrille <martin@gorgias.io>
2016-02-25 23:08:59 +0100

    moving to 4spaces-tabs and moved bin/aito to ./aito.py

--- libaito/helpers.py
+++ libaito/helpers.py
@@ -2,28 +2,29 @@
 import re, sys, os
 
 def getTestSets(package):
-  """
-  Get test sets from files in ./test_sets/
-  """
-  if '/' in package:
-    package_path, package_name = package.rsplit('/', 1)
-  else:
-    package_path = '.'
-    package_name = package
-  sys.path.append(os.path.realpath(package_path))
+    """
+    Get test sets from files in ./test_sets/
+    """
+    if '/' in package:
+        package_path, package_name = package.rsplit('/', 1)
+    else:
+        package_path = '.'
+        package_name = package
+    sys.path.append(os.path.realpath(package_path))
 
-  try:
-    __package__ = __import__(package_name)
-  except:
-    print 'No such package.'
-    sys.exit(1)
+    try:
+        __package__ = __import__(package_name)
+    except Exception as e:
+        print e.__class__
+        print 'No such package.'
+        sys.exit(1)
 
-  test_sets = []
-  for mod_name in dir(__package__):
-    __module__ = getattr(__package__, mod_name)
-    for class_name in dir(__module__):
-      if re.match('Set*', class_name):
-        obj = getattr(__module__, class_name)
-        test_sets.append(obj())
+    test_sets = []
+    for mod_name in dir(__package__):
+        __module__ = getattr(__package__, mod_name)
+        for class_name in dir(__module__):
+            if re.match('Set*', class_name):
+                obj = getattr(__module__, class_name)
+                test_sets.append(obj())
 
-  return test_sets
+    return test_sets
